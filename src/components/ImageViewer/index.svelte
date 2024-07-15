5e8fb216cf02d1ee84885d0fae9ad596d9b0eb38
William Moore <w.moore@dundee.ac.uk>
2024-07-15 20:59:03 +0100

    render_image viewer with basic zoom controls

--- src/components/ImageViewer/index.svelte
+++ src/components/ImageViewer/index.svelte
@@ -3,39 +3,61 @@
 
 	let innerWidth = 0;
 	let innerHeight = 0;
+    let zoom = 100;
 
-	$: condition = innerWidth * 1.33 <= innerHeight;
-    $: imgWidth = innerWidth - 20;
+	const THUMBHEIGHT = 50; //approx
+	$: viewportRatio = innerWidth / (innerHeight - THUMBHEIGHT);
+	let imageRatio = imgData.size.width / imgData.size.height;
+	$: imgWiderThanViewport = imageRatio > viewportRatio;
+	console.log('imgTallerThanViewport', imgWiderThanViewport);
+
+	$: imgWidth = zoom / 100 * (imgWiderThanViewport ? innerWidth : (innerHeight - THUMBHEIGHT) * imageRatio);
+	$: imgHeight = zoom / 100 * (imgWiderThanViewport ? innerWidth / imageRatio : innerHeight - THUMBHEIGHT);
+
+
 </script>
 
+<!-- TODO: bind to viewport element instead of window? -->
 <svelte:window bind:innerWidth bind:innerHeight />
-
 
 <img
 	class="preview"
 	style:--viewtransitionkey="image-{imgData.id}"
 	style:background-image="url('https://idr.openmicroscopy.org/webclient/render_thumbnail/{imgData.id}/')"
-    style:width="{imgWidth}px"
-    style:height="{imgWidth}px"
+	style:width="{imgWidth}px"
+	style:height="{imgHeight}px"
+	style:top="0px"
+	style:left="0px"
 	alt="Thumbnail of {imgData.meta.Name}"
-	src="https://idr.openmicroscopy.org/webclient/render_thumbnail/{imgData.id}/"
+	src="https://idr.openmicroscopy.org/webclient/render_image/{imgData.id}/"
 />
 
-<p>Inner Width: {innerWidth}</p>
-<p>Inner Height: {innerHeight}</p>
-<p>condition: {condition}</p>
-
+<div class="dims">
+	<p>Inner Width: {innerWidth}</p>
+	<p>Inner Height: {innerHeight}</p>
+    <p>imgWiderThanViewport: {imgWiderThanViewport}</p>
+    <p>imgWidth: {imgWidth}</p>
+    <p>imgHeight: {imgHeight}</p>
+    <p>Zoom: {zoom}</p>
+    <button on:click={() =>zoom += 10}>+</button>
+    <button on:click={() =>zoom -= 10}>-</button>
+</div>
 
 <style>
 	.preview {
 		/* min-width: 100%; */
 		position: absolute;
-        top: 10px;
-        left: 10px;
 		background-size: cover;
 		background-repeat: no-repeat;
 	}
 	img {
 		view-transition-name: var(--viewtransitionkey);
+	}
+
+	.dims {
+		position: absolute;
+        top: 100px;
+        left: 10px;
+        background-color: white;
 	}
 </style>
